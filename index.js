22e825d6f7e7e39719739955ef3cc968441619b6
Yaneth Contreras <villalobosyaneth246@gmail.com>
2022-12-12 10:31:00 -0600

    funcionalidad en java

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // Obtener informacion de la forma
-const nombre =document.getElementById("nombre");
-const correo =document.getElementById("correo");
+const nombre = document.getElementById("name");
+const correo = document.getElementById("email");
 const trabajo = document.getElementById("trabajo");
-const telefono =document.getElementById("telefono");
-const sobreMi =document.getElementById("sobre mi");
+const telefono = document.getElementById("telefono");
+const sobreMi = document.getElementById("about");
 
 // llamar la forma
 const form = document.getElementById("forma");
@@ -12,44 +12,47 @@ const form = document.getElementById("forma");
 const contenedor = document.getElementById("contenedor");
 
 //crear event listener para la forma
-    form.addEventListener('submit',(e) => {
-      //Evitar la accion por defecto
-      e.preventDefault();
+form.addEventListener("submit", (e) => {
+  //Evitar la accion por defecto
+  e.preventDefault();
 
-      //crear objeto que tenga los valores de la forma
-      const formulario = {
-        name:nombre.Value,
-        gmail: correo.value,
-        job: trabajo.value,
-        cell: telefono.value,
-        about: sobreMi.value,
-      }
+  //crear objeto que tenga los valores de la forma
+  const formulario = {
+    name: nombre.value,
+    email: correo.value,
+    job: trabajo.value,
+    tel: telefono.value,
+    about: sobreMi.value,
+  };
 
-      const { name, gmail, job, cell, about } = formulario;
+  const { name, email, job, tel, about } = formulario;
 
-      const cuadroHtmlConInfo = crearCuadroDeForms
-      (name, gmail, job, cell, about);
+  const cuadroHtmlConInfo = crearCuadroDeForma(name, email, job, tel, about);
 
-      contenedor.insertAdjacentHTML('beforeend',cuadroHtmlConInfo);
-      
-      //dar la orden de submit
-      //event.submit();
+  contenedor.insertAdjacentHTML("beforeend", cuadroHtmlConInfo);
 
-    });
+  //dar la orden de submit
+  //event.submit();
+});
 
-    //crear una funcion que nos retorne contenido HTML dinamicamente
-    function crearCuadroDeForma(name, gmail, job, cell, about){
-        const cuadroHtml =`
+//crear una funcion que nos retorne contenido HTML dinamicamente
+function crearCuadroDeForma(name, email, job, tel, about) {
+  const cuadroHtml = `
+    <div class="contenedor">
 
-        <div class="contenedor">
-          <h1 class="contenedor__nombre">${name}</h1>
-          <p class="contenedor__p">${gmail}</p>
-          <p class="contenedor__p">${job}</p>
-          <p class="contenedor__p">${cell}</p>
-          <p class="contenedor__p">${about}</p>
-      </div>
-     `
-     return cuadroHtml
-    }
-        
-    
+    <div id="contenedor">
+    <div class="caja-1">
+      <img src="img.png" class="img"/>
+      <h1 class="contenedor__nombre">${name}</h1>
+      <P>Front End</P>
+    </div>
+
+
+      <p class="contenedor__p">${email}</p>
+      <p class="contenedor__p">${job}</p>
+      <p class="contenedor__p">${tel}</p>
+      <p class="contenedor__p">${about}</p>
+    </div>
+     `;
+  return cuadroHtml;
+}
